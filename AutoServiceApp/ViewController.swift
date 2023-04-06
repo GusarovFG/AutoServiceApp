f4881f8c551562dbe7d5463a5e8d85b4405f2535
gusarovFG <gusarovfg@gmail.com>
2023-04-06 17:15:12 +0400

    Initial Commit

---
+++ AutoServiceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoServiceApp
+//
+//  Created by Фаддей Гусаров on 4/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
